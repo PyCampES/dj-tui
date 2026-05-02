6f59307803f22937a17d046052be187dab0bd89b
Siro Moreno <siro.moreno.martin@gmail.com>
2026-05-02 19:14:29 +0200

    audio structure in class

--- src/dj_tui/audio.py
+++ src/dj_tui/audio.py
@@ -1,5 +1,7 @@
+from typing_extensions import Never
 from enum import Enum, IntEnum, auto
 from pathlib import Path
+import pygame
 
 
 class Deck(IntEnum):
@@ -11,36 +13,51 @@ class Action(Enum):
     PLAY = auto()
     PAUSE = auto()
 
+class AudioController():
 
-def play_pause(action: Action, deck: Deck) -> None:
-    """Play and pause action on a specific deck.
+    def check_audio_mixer_started(self,):
+        '''Check if pygame audio mixer is running, start it
+        if it is not. 
+        Returns the two audio channels.
+        '''
+        if pygame.mixer.get_init() is None:
+            pygame.mixer.init()
+            self.chan_0 = pygame.mixer.Channel(0)
+            self.chan_1 = pygame.mixer.Channel(1)
 
-    Params:
-        action: Action.PLAY or Action.PAUSE.
-        deck: Deck.LEFT or Deck.RIGHT.
-    """
-    print(f"MIDI clicked on {action} on deck {deck}")
+    def play_pause(self, action: Action, deck: Deck) -> None:
+        """Play and pause action on a specific deck.
 
-
-def set_volume(value: int, deck: Deck) -> None:
-    """Set volume on a specific deck.
-
-    Params:
-        value: Volume from 0 to 126.
-        deck: Deck.LEFT or Deck.RIGHT.
-    """
-    print(f"MIDI set volume to {value} on deck {deck}")
-
-def load_song(path: Path, deck: Deck) -> None:
-    """Load an audio file to a specific deck.
-
-    Params:
-        path: Path of the song to be loaded.
-        deck: Deck.LEFT or Deck.RIGHT.
-    """
-    if deck is Deck.LEFT:
+        Params:
+            action: Action.PLAY or Action.PAUSE.
+            deck: Deck.LEFT or Deck.RIGHT.
+        """
+        self.check_audio_mixer_started()
         ...
-    elif deck is Deck.RIGHT:
+
+
+    def set_volume(self, value: int, deck: Deck) -> None:
+        """Set volume on a specific deck.
+
+        Params:
+            value: Volume from 0 to 126.
+            deck: Deck.LEFT or Deck.RIGHT.
+        """
+        self.check_audio_mixer_started()
         ...
-    else:
-        raise ValueError("Invalid deck")
+
+
+    def load_song(self, path: Path, deck: Deck) -> None:
+        """Load an audio file to a specific deck.
+
+        Params:
+            path: Path of the song to be loaded.
+            deck: Deck.LEFT or Deck.RIGHT.
+        """
+        self.check_audio_mixer_started()
+        if deck is Deck.LEFT:
+            ...
+        elif deck is Deck.RIGHT:
+            ...
+        else:
+            raise ValueError("Invalid deck")
